421aa919bffa49c37ea5ba50b9664000fc104e2d
TheDidgeridooMan <emile.turcotte1608@gmail.com>
2022-11-29 20:33:23 -0500

    lol no work

--- js/submit.js
+++ js/submit.js
@@ -2,17 +2,15 @@
 window.addEventListener("load", function() {
 	const form = document.getElementById('cForm');
 	form.addEventListener("submit", function(e) {
-		setTimeout(function() {
-			e.preventDefault();
-			const data = new FormData(form);
-			const action = e.target.action;
-			fetch(action, {
-				method: 'POST',
-				body: data,
-			})
-			.then(() => {
-				alert("Success!");
-			})
-		}, 3000);
+		e.preventDefault();
+		const data = new FormData(form);
+		const action = e.target.action;
+		fetch(action, {
+			method: 'POST',
+			body: data,
+		})
+		.then(() => {
+			alert("Success!");
+		})
 	})
 })
